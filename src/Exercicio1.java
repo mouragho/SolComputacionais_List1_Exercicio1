1a8d53726fba6f1da363a2ec44a1f0140fde8996
mouragho <gabriel.moura@outlook.com.br>
2022-10-07 23:22:38 -0300

    Criação do projeto Exercicio1

---
+++ src/Exercicio1.java
@@ -0,0 +1,6 @@
+//Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+public class Exercicio1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Alô Mundo!"); //Imprime a mensagem na tela
+    }
+}
